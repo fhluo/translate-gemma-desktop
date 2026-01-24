b6024e984a881e50ba2fd83b0c8c9217198a12d3
fhluo <20843637+fhluo@users.noreply.github.com>
2026-01-24 20:22:42 +0800

    feat: add `LocaleSelector`

---
+++ src/locale_selector.rs
@@ -0,0 +1,108 @@
+use crate::assets::Icons;
+use gpui::{
+    Action, Context, Corner, FocusHandle, InteractiveElement, IntoElement, Render, Styled, Window,
+};
+use gpui_component::button::{Button, ButtonVariants};
+use gpui_component::menu::DropdownMenu;
+use gpui_component::{gray_500, Sizable};
+use icu_experimental::displaynames::LocaleDisplayNamesFormatter;
+use rust_i18n::{available_locales, t};
+use schemars::JsonSchema;
+use serde::{Deserialize, Serialize};
+use std::fmt::{Display, Formatter};
+
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize, JsonSchema, Action)]
+pub struct ChangeLocale(pub String);
+
+#[derive(Debug, Clone)]
+pub struct Locale {
+    id: String,
+    display_name: Option<String>,
+}
+
+impl Display for Locale {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.display_name.as_ref().unwrap_or(&self.id))
+    }
+}
+
+impl Locale {
+    pub fn new(locale: impl Into<String>) -> Self {
+        let locale = locale.into();
+
+        Locale {
+            display_name: {
+                locale
+                    .parse::<icu_locale::Locale>()
+                    .ok()
+                    .and_then(|locale| {
+                        match LocaleDisplayNamesFormatter::try_new(
+                            locale.clone().into(),
+                            Default::default(),
+                        ) {
+                            Ok(display_name) => Some(display_name.of(&locale).into_owned()),
+                            Err(err) => {
+                                eprintln!(
+                                    "failed to create locale display names formatter for {}: {}",
+                                    locale, err
+                                );
+                                None
+                            }
+                        }
+                    })
+            },
+            id: locale,
+        }
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct LocaleSelector {
+    locales: Vec<Locale>,
+    focus_handle: FocusHandle,
+}
+
+impl LocaleSelector {
+    pub fn new(focus_handle: FocusHandle) -> LocaleSelector {
+        let locales = available_locales!()
+            .into_iter()
+            .map(|locale| Locale::new(locale))
+            .collect::<Vec<_>>();
+
+        LocaleSelector {
+            locales,
+            focus_handle,
+        }
+    }
+}
+
+impl Render for LocaleSelector {
+    fn render(&mut self, _: &mut Window, _: &mut Context<Self>) -> impl IntoElement {
+        Button::new("language-button")
+            .track_focus(&self.focus_handle)
+            .small()
+            .ghost()
+            .icon(Icons::Languages)
+            .text_color(gray_500())
+            .tooltip(t!("language"))
+            .dropdown_menu({
+                let this = self.clone();
+
+                move |mut menu, _, _| {
+                    menu = menu.action_context(this.focus_handle.clone());
+                    let selected = rust_i18n::locale().to_owned();
+
+                    for locale in &this.locales {
+                        menu = menu.menu_with_check(
+                            locale.to_string(),
+                            selected == locale.id,
+                            Box::new(ChangeLocale(locale.id.clone())),
+                        );
+                    }
+
+                    menu
+                }
+            })
+            .anchor(Corner::TopRight)
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,42 @@
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
+#[macro_use]
+extern crate rust_i18n;
+
 mod assets;
 mod language;
 mod language_selector;
+mod locale_selector;
 mod prompt;
 
 use crate::assets::Assets;
 use crate::language_selector::LanguageSelector;
+use crate::locale_selector::{ChangeLocale, LocaleSelector};
 use gpui::{
-    div, prelude::*, px, size, Application, Bounds, Entity, Window, WindowBounds, WindowOptions,
+    div, prelude::*, px, size, Application, Bounds, Entity, FocusHandle, Focusable,
+    Window, WindowBounds, WindowOptions,
 };
 use gpui_component::{Root, TitleBar};
 use icu_locale::locale;
+use rust_i18n::set_locale;
+
+i18n!("locales", fallback = "en");
 
 struct TranslateApp {
+    locale_selector: Entity<LocaleSelector>,
+
     source_language_selector: Entity<LanguageSelector>,
     target_language_selector: Entity<LanguageSelector>,
+
+    focus_handle: FocusHandle,
 }
 
 impl TranslateApp {
     fn new(window: &mut Window, cx: &mut Context<Self>) -> Self {
+        let focus_handle = cx.focus_handle();
+
+        let locale_selector = cx.new(|_| LocaleSelector::new(focus_handle.clone()));
+
         let source_language_selector =
             cx.new(|cx| LanguageSelector::new(locale!("zh-Hans"), window, cx));
 
@@ -25,20 +44,52 @@ impl TranslateApp {
             cx.new(|cx| LanguageSelector::new(locale!("zh-Hans"), window, cx));
 
         TranslateApp {
+            locale_selector,
             source_language_selector,
             target_language_selector,
+            focus_handle,
         }
+    }
+
+    fn on_action_change_locale(
+        &mut self,
+        ChangeLocale(locale): &ChangeLocale,
+        window: &mut Window,
+        cx: &mut Context<Self>,
+    ) {
+        self.locale_selector.update(cx, |_, _| {
+            set_locale(locale);
+            window.refresh();
+        });
     }
 }
 
 impl Render for TranslateApp {
-    fn render(&mut self, _: &mut Window, _: &mut Context<Self>) -> impl IntoElement {
+    fn render(&mut self, _: &mut Window, cx: &mut Context<Self>) -> impl IntoElement {
         div()
             .w_full()
             .h_full()
             .flex()
             .flex_col()
-            .child(TitleBar::new())
+            .child(
+                TitleBar::new().items_center().child(
+                    div()
+                        .track_focus(&self.focus_handle)
+                        .on_action(cx.listener(Self::on_action_change_locale))
+                        .flex()
+                        .flex_row()
+                        .flex_1()
+                        .child(
+                            div()
+                                .flex()
+                                .flex_row()
+                                .h_full()
+                                .ml_auto()
+                                .mr_3()
+                                .child(self.locale_selector.clone()),
+                        ),
+                ),
+            )
             .child(
                 div()
                     .w_full()
